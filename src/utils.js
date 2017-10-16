289bb35f7118fb36fc0505a4183eee5c9d804595
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-10-16 16:22:21 -0500

    changed autostart method

--- src/utils.js
+++ src/utils.js
@@ -1,24 +1,16 @@
 obtain(['fs', 'child_process'], (fs, { spawn })=> {
   exports.copyConfigFile = (src, dest, fillObj)=> {
-    fs.writeFileSync(dest, '');
-    fs.readFileSync(src).toString().split('\n').forEach(function(line) {
-      if (fillObj) {
-        var reg = /\${([^}]*)}/;
-        var ln = line.toString();
-        if (ln.search(reg) >= 0) {
-          let repl = ln.replace(reg, (match, p1, offset, string)=> {
-            if (fillObj[p1]) return fillObj[p1];
-            else return '';
-          });
-          fs.appendFileSync(dest, repl + '\n');
-        } else {
-          fs.appendFileSync(dest, line.toString() + '\n');
-        }
-      } else fs.appendFileSync(dest, line.toString() + '\n');
-    });
+    var orig = fs.readFileSync(src).toString();
+    for (var key in fillObj) {
+      if (fillObj.hasOwnProperty(key)) {
+        orig = orig.replace('${' + key + '}', fillObj[key]);
+      }
+    }
+
+    fs.writeFileSync(dest, orig);
   };
 
-  exports.call = function(cmd) {
+  exports.call = function (cmd) {
     var _this = this;
     _this.command = cmd;
     _this.running = false;
