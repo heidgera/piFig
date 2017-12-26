2b7cf63dbde1ee68dc32e20e3e60f3299ca09f42
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-12-26 15:29:06 -0600

    changes for keyboard

--- src/wifi.js
+++ src/wifi.js
@@ -1,6 +1,6 @@
 obtain(['fs', `${__dirname}/utils.js`], (fs, utils)=> {
   var writeWPASupplicant = (ssid, pass)=> {
-    utils.copyConfigFile('./configFiles/wpa_supplicant_default.conf',
+    utils.copyConfigFile(`${__dirname}/configFiles/wpa_supplicant_default.conf`,
                           '/etc/wpa_supplicant/wpa_supplicant.conf',
                           { SSID: ssid, PASSWORD: pass });
   };
