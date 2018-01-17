0b739bb162b1fbc008b0d72a5a7cf7904752a503
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-01-17 14:11:54 -0600

    wifi fix

--- src/wifi.js
+++ src/wifi.js
@@ -1,6 +1,6 @@
 obtain(['fs', `${__dirname}/utils.js`], (fs, utils)=> {
   var writeWPASupplicant = (ssid, pass)=> {
-    utils.copyConfigFile(window.µdir + '/piFig/configFiles/wpa_supplicant_default.conf',
+    utils.copyConfigFile('./configFiles/wpa_supplicant_default.conf',
                           '/etc/wpa_supplicant/wpa_supplicant.conf',
                           { SSID: ssid, PASSWORD: pass });
   };
