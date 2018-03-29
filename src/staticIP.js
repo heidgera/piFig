0b9eb3eb13350636afa8595e743e4c2a48e711f4
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-03-29 10:56:44 -0500

    added interfaces file for static ip

--- src/staticIP.js
+++ src/staticIP.js
@@ -3,8 +3,8 @@ obtain(['fs', `${__dirname}/utils.js`], (fs, utils)=> {
     utils.copyConfigFile(`${__dirname}/../configFiles/dhcpcd_staticIP.conf`,
                           '/etc/dhcpcd.conf',
                           { STATIC_IP: ip });
-    utils.copyConfigFile(`${__dirname}/../configFiles/interfaces_staticIP.conf`,
-                          '/etc/dhcpcd.conf');
+    utils.copyConfigFile(`${__dirname}/../configFiles/interfaces_staticIp.conf`,
+                          '/etc/network/interfaces');
   };
 
   exports.configure = (ip)=> {
